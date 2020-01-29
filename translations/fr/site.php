860e5cf3e0f7da250eed50d799985bdee0735772
Ian Reid Langevin <ian@3ejoueur.com>
2020-01-29 11:06:28 -0500

    A - add translation folder and mod to git ignore

---
+++ translations/fr/site.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+  "View" => "Consulter",
+  "Read more" => "Lire en entier",
+  "Learn more" => "En savoir plus",
+  "F j, Y" => "j F Y",
+  "Switch language" => "Changer la langue",
+  "Address" => "Coordonnées",
+  "Contact" => "Contact",
+  "Contact us" => "Contactez-nous",
+  "Send" => "Envoyer",
+  "Categories" => "Catégories",
+  "All news" => "Toute l'actualité",
+  "Website" => "Site Internet",
+  "There are no news in this category at the moment." => "Il n'y a pas d'article dans cette catégorie pour l'instant.",
+];
