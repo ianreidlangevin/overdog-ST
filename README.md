4e2eaebcbddb916439b69e493e16c8c1e107146f
Ian Reid Langevin <ian@3ejoueur.com>
2020-10-16 15:59:20 -0400

    M - fix typo in spacing mixin changes

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 
 # Overdog - A scaffolding project for Craft CMS 3
 
-### Official documentation :
+## Version
+1.0.1
+
+## Official documentation :
 _Following documentation is only in French for the moment_
 
 https://public-3e-joueur.gitlab.io/overdog-docs/
--- src/js/parts/lazysizes.js
+++ src/js/parts/lazysizes.js
@@ -6,21 +6,20 @@
   Delay image loading on scroll
   Docs : https://github.com/aFarkas/lazysizes
 
+  Overdog : See the lazyload utility in SCSS folder for more details
+
 */
 
 import lazySizes from 'lazysizes';
 
 /*
-* Set your options here if you ned
+* Set your options here if you need, defaults should be good
 * Example
 *
 
 lazySizes.init({
     hFac: 10,
     loadMode: 1,
-    lazyClass: 'js-lazy',
-    loadingClass: 'is-loading',
-    loadedClass: 'is-loaded',
     throttleDelay: 100,
 });
 
