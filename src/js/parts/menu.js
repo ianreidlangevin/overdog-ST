79b82fd7ec790eea4564fd55a1612e8bb3a8f6c6
Ian Reid Langevin <ian@3ejoueur.com>
2020-11-20 15:07:56 -0500

    F - breakpoint in body size and - A aria to menu

--- src/js/parts/menu.js
+++ src/js/parts/menu.js
@@ -9,5 +9,6 @@ const menuButton = document.querySelector(".navbar__menu-button")
 if(menuButton){
   menuButton.addEventListener("click", () => {
     document.body.classList.toggle("mega-nav--open")
+    menuButton.setAttribute("aria-expanded", menuButton.getAttribute("aria-expanded") === "false" ? "true" : "false")
   })
 }
--- src/js/parts/shrink-nav.js
+++ src/js/parts/shrink-nav.js
@@ -6,10 +6,27 @@
 
 const navbar = document.querySelector(".navbar")
 
-window.onscroll = () => {
-  if (document.documentElement.scrollTop > 100) {
-    navbar.classList.add("navbar--shrink")
-  } else {
-    navbar.classList.remove("navbar--shrink")
+if (navbar) {
+  let ticking = false
+
+  /* eventListener and thicking for performance */
+  window.addEventListener("scroll", event => {
+    if (!ticking) {
+      window.requestAnimationFrame(function () {
+        shrinkNav()
+        ticking = false
+      })
+    }
+
+    ticking = true
+  })
+
+  /* Function to show or hide navbar */
+  function shrinkNav () {
+    if (document.documentElement.scrollTop > 100) {
+      navbar.classList.add("navbar--shrink")
+    } else {
+      navbar.classList.remove("navbar--shrink")
+    }
   }
 }
