9506bd3a4671be8e0ddbec4323c527916e11b280
Ian Reid Langevin <ian@3ejoueur.com>
2020-11-20 15:08:56 -0500

    A - version bump to 1.1.3

--- src/js/parts/menu.js
+++ src/js/parts/menu.js
@@ -6,7 +6,7 @@
 
 const menuButton = document.querySelector(".navbar__menu-button")
 
-if(menuButton){
+if (menuButton) {
   menuButton.addEventListener("click", () => {
     document.body.classList.toggle("mega-nav--open")
     menuButton.setAttribute("aria-expanded", menuButton.getAttribute("aria-expanded") === "false" ? "true" : "false")
