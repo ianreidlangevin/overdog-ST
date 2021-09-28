59a8019a78ec63f677e3744e635784c4570d4b74
Ian Reid Langevin <ian@3ejoueur.com>
2021-09-28 14:43:03 -0400

    M - french apostrophe character in translation

--- translations/fr/site.php
+++ translations/fr/site.php
@@ -11,9 +11,9 @@ return [
   "Contact us" => "Contactez-nous",
   "Send" => "Envoyer",
   "Categories" => "Catégories",
-  "All news" => "Toute l'actualité",
-  "There are no news in this category at the moment." => "Il n'y a pas de nouvelle dans cette catégorie pour l'instant.",
+  "All news" => "Toute l’actualité",
+  "There are no news in this category at the moment." => "Il n’y a pas de nouvelle dans cette catégorie pour l’instant.",
   /* 404 */
   "Sorry, wrong path!" => "Désolé, mauvais chemin!",
-  "This page does not exist! Please use the main menu to go to another page." => "Cette page n'existe pas. Veuillez utiliser le menu principal pour naviguer.",
+  "This page does not exist! Please use the main menu to go to another page." => "Cette page n’existe pas. Veuillez utiliser le menu principal pour naviguer.",
 ];
