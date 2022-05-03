746318f0362cd3d88d7dfa9178196338242cb222
Ian Reid Langevin <ian@3ejoueur.com>
2022-05-03 11:02:12 -0400

    M - clean-up base config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,22 +15,19 @@ module.exports = {
       --------------------------------------------------------------------------
       */
       fluidCSS: {
-         // elements
-         "fluid-icon-h": { prop: ["height"], minSize: "48px", maxSize: "62px", minScreen: "sm", maxScreen: "xl" },
-         "fluid-icon-w": { prop: ["width"], minSize: "48px", maxSize: "62px", minScreen: "sm", maxScreen: "xl" },
-         "fluid-arguments-icon": { prop: "width", minSize: "40px", maxSize: "68px", minScreen: "sm", maxScreen: "xxl" },
+         // Exemple for a logo
          "fluid-logo": { prop: "width", minSize: "30px", maxSize: "42px", minScreen: "sm", maxScreen: "xxl" },
-         "fluid-footer-web-intel": { prop: "width", minSize: "120px", maxSize: "340px", minScreen: "sm", maxScreen: "xxxl" },
-         "fluid-footer-stamp": { prop: "width", minSize: "70px", maxSize: "142px", minScreen: "sm", maxScreen: "xxxl" },
-         "fluid-testimonial-storm": { prop: "width", minSize: "46px", maxSize: "92px", minScreen: "sm", maxScreen: "xxxl" },
-         "fluid-testimonial-quote": { prop: "width", minSize: "46px", maxSize: "92px", minScreen: "sm", maxScreen: "xxxl" },
-         // font
-         "fluid-title-page": { prop: "font-size", minSize: "32px", maxSize: "92px", minScreen: "500px", maxScreen: "xxl" },
-         "fluid-title-small": { prop: "font-size", minSize: "10px", maxSize: "14px", minScreen: "sm", maxScreen: "xxl" },
-         "fluid-title-block": { prop: "font-size", minSize: "16px", maxSize: "18px", minScreen: "sm", maxScreen: "xxl" },
-         "fluid-title-section": { prop: "font-size", minSize: "24px", maxSize: "38px", minScreen: "sm", maxScreen: "xxl" },
-         "fluid-text-punchline": { prop: "font-size", minSize: "15px", maxSize: "20px", minScreen: "sm", maxScreen: "xxl" },
-         "fluid-text-accroche": { prop: "font-size", minSize: "16px", maxSize: "22px", minScreen: "sm", maxScreen: "xxl" }
+         // Exemple for a title - use it with the @apply directive in a CSS file if you want to set line-height, spacing, etc. and create a global class
+         "fluid-title-page": { prop: "font-size", minSize: "32px", maxSize: "92px", minScreen: "500px", maxScreen: "xxl" }
+      },
+      /*
+      -------------------------------------
+      FONTS FAMILY
+      -------------------------------------
+      */
+      fontFamily: {
+         body: ["Helvetica", "arial", "sans-serif"],
+         title: ["Helvetica", "arial", "sans-serif"]
       },
       /*
       -------------------------------------
@@ -41,12 +38,9 @@ module.exports = {
          sm: ["14px", "20px"],
          base: ["16px", "24px"],
          lg: ["20px", "28px"],
+         // exemple with lineHeight - See docs for infos
          xl: ["24px", { lineHeight: "32px" }],
          xxl: ["32px", { lineHeight: "48px" }]
-      },
-      fontFamily: {
-         body: ["Helvetica", "arial", "sans-serif"],
-         title: ["Helvetica", "arial", "sans-serif"]
       },
       /*
       -------------------------------------
@@ -54,15 +48,15 @@ module.exports = {
       -------------------------------------
       */
       colors: {
-         transparent: "transparent",
-         foreground: "hsl(212, 100%, 99%)",
+         transparent: "transparent", // NEEDED BY TAILWIND
          white: "hsl(0, 0%, 100%)",
+         black: "hsl(0, 0%, 0%)",
          body: "hsl(210, 14%, 40%)",
-         red: {
+         primary: {
             DEFAULT: "hsl(356, 85%, 57%)",
             90: "hsl(356, 85%, 90%)"
          },
-         blue: {
+         secondary: {
             DEFAULT: "hsl(230, 60%, 16%)",
             92: "hsl(230, 60%, 92%)",
             96: "hsl(230, 60%, 96%)",
@@ -73,10 +67,8 @@ module.exports = {
             40: "hsl(210, 14%, 40%)",
             65: "hsl(210, 14%, 65%)",
             70: "hsl(210, 14%, 70%)",
-            80: "hsl(210, 14%, 80%)",
-            84: "hsl(210, 14%, 84%)"
-         },
-         black: "hsl(0, 0%, 0%)"
+            80: "hsl(210, 14%, 80%)"
+         }
       },
       /*
       -------------------------------------
@@ -84,24 +76,7 @@ module.exports = {
       -------------------------------------
       */
       extend: {
-         transitionTimingFunction: {
-            "nav-in": "cubic-bezier(0, 0, 0.58, 1)",
-            "nav-out": "cubic-bezier(0.42, 0, 1, 1)",
-            "in-expo": "cubic-bezier(0.95, 0.05, 0.795, 0.035)",
-            "out-expo": "cubic-bezier(0.19, 1, 0.22, 1)"
-         },
-         transitionDuration: {
-            250: "250ms",
-            350: "350ms",
-            400: "400ms"
-         },
-         backgroundImage: {
-            "dotted-pattern-light": "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAABGdBTUEAALGPC/xhBQAAADhlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAAqACAAQAAAABAAAAIKADAAQAAAABAAAAIAAAAACPTkDJAAAAbklEQVRYCe3QywmAQAyE4YlFCIIWZCMWZSMWpCDYxL4OnvYgc/VfCCyBScIn8RBAAAEEEPi7QLgA5/2sStpbPrQt03g4swYn1DJleVKaa72HGMP8A4xlvYh/QGEPxVVL5d8bTg8BBBBAAAEEvghkxMEQCMouvFgAAAAASUVORK5CYII=')",
-            "dotted-pattern-dark": "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAIAAAD8GO2jAAAABGdBTUEAALGPC/xhBQAAADhlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAAqACAAQAAAABAAAAIKADAAQAAAABAAAAIAAAAACPTkDJAAAAh0lEQVRIDe2SsQ6AIAxEhSgTi6Of4///Cy5MmIA1JB05OrAdYShw3IVX3Hnd28rhV5r/3gyAhImIiCABKOAvgoh2qFBBCEeMUZY551Je3R8Xhh6Iu3NeZo8Z++qpIUDvmApDgJBprcqUYj7D0APhntIzb92VhhdYrRkwSYw9gKCIiIggASj4AO6SGGl4qO+oAAAAAElFTkSuQmCC')"
-         },
-         zIndex: {
-            1: "1"
-         }
+         // See Tailwind doc for info
       }
    },
    /*
