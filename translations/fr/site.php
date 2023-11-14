234835a96f906e92631c415a16ea15b2e7fb11f7
Ian Reid Langevin <ian@reidlangevin.com>
2023-11-14 15:50:37 -0500

    A - singleHome new settings - english now default

--- translations/fr/site.php
+++ translations/fr/site.php
@@ -4,20 +4,20 @@
    Adapt to your need or delete it.
 */
 return [
-  "View" => "Consulter",
-  "Read more" => "Lire en entier",
-  "Learn more" => "En savoir plus",
-  "F j, Y" => "j F Y",
-  "Switch language" => "Changer la langue",
-  "Address" => "Coordonnées",
-  "Opening hours" => "Heures d’ouverture",
-  "Contact" => "Contact",
-  "Contact us" => "Contactez-nous",
-  "Send" => "Envoyer",
-  "Categories" => "Catégories",
-  "All news" => "Toute l’actualité",
-  "There are no news in this category at the moment." => "Il n’y a pas de nouvelle dans cette catégorie pour l’instant.",
-  /* 404 */
-  "Sorry, wrong path!" => "Désolé, mauvais chemin!",
-  "This page does not exist! Please use the main menu to go to another page." => "Cette page n’existe pas. Veuillez utiliser le menu principal pour naviguer.",
+   "View" => "Consulter",
+   "Read more" => "Lire en entier",
+   "Learn more" => "En savoir plus",
+   "F j, Y" => "j F Y",
+   "Switch language" => "Changer la langue",
+   "Address" => "Coordonnées",
+   "Opening hours" => "Heures d’ouverture",
+   "Contact" => "Contact",
+   "Contact us" => "Contactez-nous",
+   "Send" => "Envoyer",
+   "Categories" => "Catégories",
+   "All news" => "Toute l’actualité",
+   "There are no news in this category at the moment." => "Il n’y a pas de nouvelle dans cette catégorie pour l’instant.",
+   /* 404 */
+   "Sorry, wrong path!" => "Désolé, mauvais chemin!",
+   "This page does not exist! Please use the main menu to go to another page." => "Cette page n’existe pas. Veuillez utiliser le menu principal pour naviguer.",
 ];
